32a60027302abd8d472b54aaeb373aeca5c92c8e
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-03-14 23:43:52 +0700

    Init commit

---
+++ portfolio.md
@@ -0,0 +1,8 @@
+---
+layout: default
+permalink: /portfolio
+---
+
+### Portfolio
+
+Not thing here yet.
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+        './_drafts/**/*.html',
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.md',
+        './*.html',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'source': ['"Source Sans Pro"', 'sans-serif'],
+            },
+        },
+    },
+    plugins: []
+}
